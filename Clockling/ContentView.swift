a2b58931dcd5880be3d7a3aada7ab661de12bb47
Lluís Merlos <llmerlos@outlook.com>
2021-07-21 22:58:43 +0200

    working template

--- Clockling/ContentView.swift
+++ Clockling/ContentView.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)    }
 }
 
 struct ContentView_Previews: PreviewProvider {
