53be710f8490e8496f1009a8dc026a05af376889
Lluís Merlos <llmerlos@outlook.com>
2021-07-21 20:05:33 +0200

    Initial XCode Commit

---
+++ Clockling/ClocklingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClocklingApp.swift
+//  Clockling
+//
+//  Created by Lluís Merlos Pieri on 21/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ClocklingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
